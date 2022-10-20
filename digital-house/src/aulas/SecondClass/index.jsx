c0dd5f5f361d5e7f6122082d07299bd92ed80bf7
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-10-19 23:33:53 -0300

    Aula 2 -componentes

---
+++ digital-house/src/aulas/SecondClass/index.jsx
@@ -0,0 +1,57 @@
+import './style.scss'
+
+export function SecondClass() {
+    return (
+        <main className='main-SecondClass'>
+            <h1>Segunda Aula</h1>
+            <h2>Site - <a href={"https://www.starplus.com/en-br"} target={"_blank"} >Star+</a> </h2>
+            <br/>
+            <h3>Componentes</h3>
+            <ul>
+                <li>
+                    <p>Header</p>
+                </li>
+                <li>
+                    <p>Banner</p>
+                </li>
+                <li>
+                    <p>Buttons</p>
+                </li>
+                <li>
+                    <p>Plans Cards</p>
+                </li>
+                <li>
+                    <p>Sections</p>
+                </li>
+                <li>
+                    <p>Accordion-Container - Perguntas Frequentes</p>
+                </li>
+                <li>
+                    <p>Footer</p>
+                </li>
+            </ul>
+            <br/>
+            <h3>Integrantes</h3>
+            <ul className='ulIntegrantes'>
+                <li>
+                    <p>Angelica Lirio</p>
+                </li>
+                <li>
+                    <p>Elivelton Alves Viveiro</p>
+                </li>
+                <li>
+                    <p>Filipe Farias</p>
+                </li>
+                <li>
+                    <p>Harry Mobbs Júnior</p>
+                </li>
+                <li>
+                    <p>J. Felipe S. Barbosa</p>
+                </li>
+                <li>
+                    <p>Narayana Brahmajyoti</p>
+                </li>
+            </ul>
+        </main>
+    )
+}
