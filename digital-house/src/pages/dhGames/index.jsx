e22de46246469fdd48b43b526a98440b2d9f5bbf
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-11-05 08:56:03 -0300

    Página DH Games

--- digital-house/src/pages/dhGames/index.jsx
+++ digital-house/src/pages/dhGames/index.jsx
@@ -1,27 +1,109 @@
-import { VerticalGamesItem } from '../../components/DhGames/VerticalGamesItem'
-import { newGames } from './games'
+import { SectionBanner } from '../../components/DhGames/SectionBanner'
+import { GamesSectionHorizontalItem } from '../../components/DhGames/GamesSectionHorizontalItem'
+import { GamesSectionVerticalItem } from '../../components/DhGames/GamesSectionVerticalItem'
+import { SectionHeader } from '../../components/DhGames/SectionHeader'
+import { cheapGames, earlyAccessGames, halloweenGames, newGames } from './games.js'
 import './style.scss'
 
 
 export function DHGames(){
+
     return (
-        <div>
-            <section>
-                <h2>New Games</h2>
-                
-                {
-                    newGames.map(
-                        sectionNewGame => {
-                            return(
-                                <VerticalGamesItem 
-                                    
-                                    item={sectionNewGame}
-                                />
+
+        <div className='dh-games-component'>
+
+            <SectionHeader />
+            <SectionBanner />
+
+            <main className='dh-games-main-content'>
+
+                <section className='games-section'>
+
+                    <h1 className='games-section-title'>New Games</h1>
+
+                    <section className='games'>  
+                        {
+                            newGames.map( 
+                                game => {
+                                    return(
+                                        <GamesSectionVerticalItem 
+                                            type='vertical'
+                                            game={game}
+                                        />
+                                    )
+                                }
                             )
                         }
-                    )
-                }
-            </section>
+                    </section> 
+
+                </section>
+
+                <section className='games-section'>
+
+                    <h1 className='games-section-title'>Halloween Offers</h1>
+
+                    <section className='games'>  
+                        {
+                            halloweenGames.map( 
+                                game => {
+                                    return(
+                                        <GamesSectionHorizontalItem 
+                                            type='card'
+                                            game={game}
+                                        />
+                                    )
+                                }
+                            )
+                        }
+                    </section> 
+
+                </section>
+
+                <section className='games-section'>
+
+                    <h1 className='games-section-title'>Early Access Games</h1>
+
+                    <section className='games'>  
+                        {
+                            earlyAccessGames.map( 
+                                game => {
+                                    return(
+                                        <GamesSectionVerticalItem 
+                                            type='vertical'
+                                            game={game}
+                                        />
+                                    )
+                                }
+                            )
+                        }
+                    </section> 
+
+                </section>
+
+                <section className='games-section'>
+
+                    <h1 className='games-section-title'>Cheap Games</h1>
+
+                    <section className='games'>  
+                        {
+                            cheapGames.map( 
+                                game => {
+                                    return(
+                                        <GamesSectionHorizontalItem 
+                                            type='card'
+                                            game={game}
+                                        />
+                                    )
+                                }
+                            )
+                        }
+                    </section> 
+
+                </section>
+
+
+            </main>
+
         </div>
     )
 }
