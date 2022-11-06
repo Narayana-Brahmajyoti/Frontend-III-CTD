f92f4c76579ec1fddd8a0ebe6870318df54ac033
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-11-06 20:37:12 -0300

    Aprendendo a usar UserState e a chamar o onClick no react

--- digital-house/src/lessons/SeventhClass/index.jsx
+++ digital-house/src/lessons/SeventhClass/index.jsx
@@ -1,9 +1,39 @@
+import { useState } from 'react'
 import './style.scss'
 
-export function SeventhClass(){
+export function SeventhClass() {
 
+    //let contador = 0
+    const [contador, setContador] = useState(0)
+
+    const [temaClaro, setTemaClaro] = useState(false)
+
+    function somarNumero(){
+        
+        setContador(contador + 1)
+
+    }
+
+    function mudarTema(){
+        
+        setTemaClaro(!temaClaro)
+
+    }
 
     return(
-        <main></main>
+
+        <main className={ `setima-aula-component-contador ${temaClaro ? 'lightTheme': ''}` }>
+
+            <h1>Contador</h1>
+
+            <button onClick={() => somarNumero()}>Adicionar</button>
+            <span>Número atual: {contador}</span>
+
+            <button onClick={mudarTema}>Mudar Tema</button>
+
+        </main>
+
     )
+
+
 }
