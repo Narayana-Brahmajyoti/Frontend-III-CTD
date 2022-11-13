346f9660138567d9dbc065cf501eacc21dba5017
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-11-13 09:59:40 -0300

    prof pode desconsiderar está alteração por extrapolar o prazo, não consegui fazer até ontem mas não podia ficar sem conseguir realizar a tarefa :)

--- ctd-frontend3-first-checkpoint/ctd-frontend3-first-checkpoint/src/cardComponent/Card.jsx
+++ ctd-frontend3-first-checkpoint/ctd-frontend3-first-checkpoint/src/cardComponent/Card.jsx
@@ -5,38 +5,31 @@ export function Card (props) {
 
   function lightOrDark(color) {
 
-    // Check the format of the color, HEX or RGB?
-    if(color.match(/^rgb/)) {
-  
-      // If HEX --> store the red, green, blue values in separate variables
-      color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
-  
-      let r = color[1];
-      let g = color[2];
-      let b = color[3];
-    } else {
-  
-      // If RGB --> Convert it to HEX: http://gist.github.com/983661
-      color = +("0x" + color.slice(1).replace(
-        color.length < 5 && /./g, '$&$&'
-      ));
-  
-      let r = color >> 16;
-      let g = color >> 8 & 255;
-      let b = color & 255;
-    
-      // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    // If RGB --> Convert it to HEX: http://gist.github.com/983661
+    color = +("0x" + color.slice(1).replace(
+      color.length < 5 && /./g, '$&$&'
+    ));
+
+    let r = color >> 16;
+    let g = color >> 8 & 255;
+    let b = color & 255;
+
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
     let hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
   
     // Using the HSP value, determine whether the color is light or dark
-     return hsp > 127.5
+    return hsp > 127.5
     
-    }
+    
   
   }
 
+  const tema = (cor) => {
+    return lightOrDark(cor) ? 'section-card-horizontal-light' : 'section-card-horizontal-dark'
+  }
+
   return (
-    <div className={ lightOrDark ? 'section-card-horizontal-light' : 'section-card-horizontal-dark'} style={{backgroundColor:props.cardCor.cor}}>
+    <div className={ tema(props.cardCor.cor) } style={{backgroundColor:props.cardCor.cor }}>
       <span className="section-card-horizontal-span" >{props.cardCor.nome}</span>
       <h1 className="section-card-horizontal-title">{props.cardCor.cor}</h1>
 
