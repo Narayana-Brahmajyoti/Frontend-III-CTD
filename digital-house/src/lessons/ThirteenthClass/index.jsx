cb9bb134518aa7c1bbc86f0d7197a56d26e31cca
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-11-13 09:56:54 -0300

    prof pode desconsiderar está alteração por extrapolar o prazo, não consegui fazer até ontem mas não podia ficar sem conseguir realizar a tarefa :)

---
+++ digital-house/src/lessons/ThirteenthClass/index.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+
+
+export function ThirteenthClass(){
+    
+    // const [contador, setContador] = useState()
+    
+    // useEffect(() => {
+    //     console.log('teste')
+    // }), []
+
+    // setTimeout(() => {
+
+                
+
+    // }, 2000)
+    
+    return(
+        <>
+           <h1></h1>
+        </>
+    )
+}
