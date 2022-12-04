32519adfa72c74feb16c8cf1102371cac7fed9a5
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-12-04 00:21:20 -0300

    corrigindo a estilização nas páginas de login e To-do

--- digital-house/src/pages/Login/index.jsx
+++ digital-house/src/pages/Login/index.jsx
@@ -1,8 +1,12 @@
+import './style.scss'
+
 export function Login(){
     return (
-        <main>
-            <h1 className='title has-shown'>Frontend III</h1>
-            <p className="">Eu sou o componente de Login</p>
+        <main className='login'>
+            <div>
+                <h1 className="text-title has-shown">Frontend III</h1>
+                <p className="text has-shown">Eu sou o componente de Login</p>
+            </div>
         </main>
     )
 }
